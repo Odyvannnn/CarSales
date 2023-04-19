d3883f0691f37160f2419754c35a9d5648646581
Odyvannnn <odyvannnn@gmail.com>
2023-04-19 19:21:19 +0300

    Reworked main screen. Added searching by car brand/name using spinners

--- app/src/main/java/com/main/carsales/main/HomeFragment.kt
+++ app/src/main/java/com/main/carsales/main/HomeFragment.kt
@@ -5,18 +5,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.main.carsales.R
-import com.main.carsales.ad.AdActivity
-import com.main.carsales.adapters.AdAdapter
+import com.main.carsales.ad.AdsListActivity
 import com.main.carsales.data.Ad
+import com.main.carsales.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding
+        get() = _binding!!
     private lateinit var recyclerView: RecyclerView
     private lateinit var adList: ArrayList<Ad>
     private var db = Firebase.firestore
@@ -24,58 +28,76 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        adList = arrayListOf()
-        val adAdapter = AdAdapter(adList)
-        recyclerView = view.findViewById(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(context)
-        db = FirebaseFirestore.getInstance()
-        db.collection("ads").get()
-            .addOnSuccessListener {
-                if (!it.isEmpty){
-                    for (data in it.documents){
-                        val ad: Ad? = data.toObject(Ad::class.java)
-                        if (ad != null) {
-                            if (ad.status == "published"){
-                                adList.add(ad)
-                            }
+
+
+        binding.searchButton.setOnClickListener{
+            val carBrandItem = binding.carBrand.selectedItem.toString()
+            val carModelItem = binding.carModel.selectedItem.toString()
+            val intent = Intent(context, AdsListActivity::class.java)
+            with(intent)
+            {
+                putExtra("car_brand_item", carBrandItem)
+                putExtra("car_model_item", carModelItem)
+            }
+            startActivity(intent)
+
+        }
+
+
+        val spinnerBrand = getView()?.findViewById<Spinner>(R.id.car_brand)
+        val spinnerModel = getView()?.findViewById<Spinner>(R.id.car_model)
+
+        fun adapterSpinner(array: Array<String>, spinner: Spinner) {
+            val adapterSpinner =
+                context?.let { ArrayAdapter(it, android.R.layout.simple_spinner_item, array) }
+            adapterSpinner?.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapterSpinner
+        }
+
+        if (spinnerBrand != null) {
+            adapterSpinner(resources.getStringArray(R.array.car_brands), spinnerBrand)
+        }
+
+        binding.carBrand.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                when (position) {
+                    0 -> {
+                        if (spinnerModel != null) {
+                            adapterSpinner(
+                                resources.getStringArray(R.array.audi_models), spinnerModel
+                            )
                         }
                     }
-                    recyclerView.adapter = adAdapter
-                    adAdapter.setOnItemClickListener(object : AdAdapter.OnItemClickListener{
-                        override fun onItemClick(position: Int) {
-                            val intent = Intent(context, AdActivity::class.java)
-                            with(intent){
-                                putExtra("car_brand", adList[position].car_brand)
-                                putExtra("car_model", adList[position].car_model)
-                                putExtra("price", adList[position].price)
-                                putExtra("year", adList[position].year)
-                                putExtra("mileage", adList[position].mileage)
-                                putExtra("car_drive_type", adList[position].car_drive_type)
-                                putExtra("engine_power", adList[position].engine_power)
-                                putExtra("transmission", adList[position].transmission)
-                                putExtra("pic1", adList[position].pic1)
-                                putExtra("pic2", adList[position].pic2)
-                                putExtra("pic3", adList[position].pic3)
-                                putExtra("pic4", adList[position].pic4)
-                                putExtra("pic5", adList[position].pic5)
-                                putExtra("pic6", adList[position].pic6)
-                                putExtra("pic7", adList[position].pic7)
-                                putExtra("pic8", adList[position].pic8)
-                                putExtra("pic9", adList[position].pic9)
-                                putExtra("seller_uid", adList[position].seller_uid)
-                                putExtra("city", adList[position].city)
-                            }
-                            startActivity(intent)
+                    1 -> {
+                        if (spinnerModel != null) {
+                            adapterSpinner(
+                                resources.getStringArray(R.array.bmw_models), spinnerModel
+                            )
                         }
-                    })
+                    }
+                    2 -> {
+                        if (spinnerModel != null) {
+                            adapterSpinner(
+                                resources.getStringArray(R.array.toyota_models), spinnerModel
+                            )
+                        }
+                    }
                 }
             }
-            .addOnFailureListener {
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
             }
+        }
     }
 }
